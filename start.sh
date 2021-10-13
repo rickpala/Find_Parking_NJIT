c3e7f987764caf1f153f90ca6903005832818239
Ricky Palaguachi <rickyp2413@gmail.com>
2021-10-13 00:11:48 -0400

    updated cmdline args for main

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ sudo systemctl start mongodb
 
 # Run python app
 source ./venv/bin/activate
-python3 refresher.py &
-python3 main.py -v &
+# python3 refresher.py &
+python3 main.py -v3 &
 wait
 python3 mailer.py
