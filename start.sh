e0226890db8b5cedfbfbc9ce84ee3754a760bb8c
Ricky Palaguachi <rickyp2413@gmail.com>
2021-10-15 14:12:02 -0400

    no longer rerun refresher.py on start

--- start.sh
+++ start.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-# Init mongo
-sudo systemctl enable mongodb
-sudo systemctl start mongodb
-
 # Run python app
 source ./venv/bin/activate
-# python3 refresher.py &
+refresher_pid=`pgrep -f "python3 refresher.py"`
+if pgrep -f "python3 refresher.py"; then
+    echo "refresher.py already running on ${refresher_pid}"
+else
+    python3 refresher.py &
+fi
 python3 main.py -v3 &
+
 wait
-python3 mailer.py
+python3 mailer.py  # Email me if main.py crashes
