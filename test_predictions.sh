17ed888334199048996c3f0effe675834227d13f
Ricky Palaguachi <rickyp2413@gmail.com>
2021-11-28 23:46:51 -0500

    bulk testing

---
+++ test_predictions.sh
@@ -0,0 +1,40 @@
+#! /bin/bash
+# Monday
+echo "Monday Predictions"
+python3 predictor.py TECH MON 09:30
+python3 predictor.py TECH MON 12:00
+python3 predictor.py TECH MON 14:00
+python3 predictor.py TECH MON 18:00
+
+python3 predictor.py PARK MON 09:30
+python3 predictor.py PARK MON 12:00
+python3 predictor.py PARK MON 14:00
+python3 predictor.py PARK MON 18:00
+
+# Tuesday
+echo "Tuesday Predictions"
+python3 predictor.py TECH TUES 08:00
+python3 predictor.py TECH TUES 10:00
+python3 predictor.py TECH TUES 12:00
+python3 predictor.py TECH TUES 14:00
+python3 predictor.py TECH TUES 18:00
+
+python3 predictor.py PARK TUES 08:00
+python3 predictor.py PARK TUES 10:00
+python3 predictor.py PARK TUES 12:00
+python3 predictor.py PARK TUES 14:00
+python3 predictor.py PARK TUES 18:00
+
+# Friday
+echo "Friday Predictions"
+python3 predictor.py TECH FRI 08:00
+python3 predictor.py TECH FRI 10:00
+python3 predictor.py TECH FRI 12:00
+python3 predictor.py TECH FRI 14:00
+python3 predictor.py TECH FRI 18:00
+
+python3 predictor.py PARK FRI 08:00
+python3 predictor.py PARK FRI 10:00
+python3 predictor.py PARK FRI 12:00
+python3 predictor.py PARK FRI 14:00
+python3 predictor.py PARK FRI 18:00
